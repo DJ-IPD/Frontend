bd349b25a34cc16d3654deb7672c7aa37e031787
IPD <mihirvora1053@gmail.com>
2023-07-25 14:56:43 +0530

    Second Commit Home Screen Done

--- screens/Home.js
+++ screens/Home.js
@@ -1,14 +1,90 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View,Image, SafeAreaView } from 'react-native';
+import { StatusBar } from "expo-status-bar";
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  SafeAreaView,
+  Switch,
+  TextInput,
+  TouchableOpacity,
+} from "react-native";
+import { useState } from "react";
+import Toggle from "react-native-toggle-element";
 
 export default function App() {
+  const [toggleValue, setToggleValue] = useState(false);
+  const [text, onChangeText] = useState("Useless Text");
+  const [number, onChangeNumber] = useState("");
   return (
     <View style={styles.container}>
-        <Image
-            source={require("../assets/home_img.jpg")}
-            style={styles.homeimg}
-          />
-          
+      <Image
+        source={require("../assets/home_img.png")}
+        style={styles.homeimg}
+      />
+      <View style={styles.greet}>
+        <Text style={{ fontSize: 24, fontWeight: "bold" }}>
+          Welcome to Farmvest
+        </Text>
+      </View>
+
+      <View style={styles.toggle}>
+        <Toggle
+          value={toggleValue}
+          onPress={(val) => setToggleValue(val)}
+          trackBar={{
+            width: 85,
+            height: 40,
+            radius: 25,
+            activeBackgroundColor: "#42BD4E",
+            inActiveBackgroundColor: "#5A5A5A",
+          }}
+          thumbButton={{
+            width: 35,
+            height: 35,
+            borderWidth: 5,
+            inActiveBackgroundColor: "#ffffff",
+            activeBackgroundColor: "#fffffff",
+          }}
+        />
+        {toggleValue ? (
+          <Text style={{ fontSize: 20, fontWeight: "bold", margin: 20 }}>
+            Farmer
+          </Text>
+        ) : (
+          <Text style={{ fontSize: 20, fontWeight: "bold", margin: 20 }}>
+            Labourer
+          </Text>
+        )}
+      </View>
+      <View>
+        <TextInput
+          style={styles.input}
+          onChangeText={onChangeNumber}
+          keyboardType="phone-pad"
+          placeholder="Enter phone number"
+          value={number}
+          maxLength={10}
+          fontSize={16}
+          padding={15}
+        />
+         <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              login(email, password);
+            }}
+          >
+            <Text style={styles.text}>Login</Text>
+          </TouchableOpacity>
+          <TouchableOpacity
+            style={styles.button}
+            onPress={() => {
+              login(email, password);
+            }}
+          >
+            <Text style={styles.text}>Sign Up</Text>
+          </TouchableOpacity>
+      </View>
     </View>
   );
 }
@@ -16,13 +92,44 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#F0F0F3',
-    flexDirection:"row",
-    justifyContent: 'center',
+    backgroundColor: "#F0F0F3",
   },
-  homeimg:{
-    marginTop:"18%",
+  homeimg: {
+    marginTop: "18%",
     width: 99,
-    height: 189
+    height: 189,
+    marginHorizontal: "39%",
+  },
+  greet: {
+    marginTop: "5%",
+    alignItems: "center",
+  },
+  toggle: {
+    flexDirection: "row",
+    marginTop: "10%",
+    marginLeft: "5%",
+  },
+  input: {
+    height: 66,
+    margin: 12,
+    borderWidth: 3,
+    borderRadius:20,
+    padding: 10,
+    letterSpacing:1.3
+  },
+  button: {
+    backgroundColor: "#21212F",
+    padding: 10,
+    height:66,
+    marginHorizontal: 12,
+    borderRadius: 24,
+    marginTop:"3%",
+    justifyContent:"center"
+  },
+  text:{
+    color:"#ffffff",
+    textAlign: "center",
+    fontSize: 20,
+    fontWeight: "bold",
   }
 });
