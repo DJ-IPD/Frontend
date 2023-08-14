b29702a06cba47d435727f4942bb933a18138bca
Mihir <mihirvora1053@gmail.com>
2023-08-14 15:00:26 +0530

    Home Screen without news component

--- screens/Home.js
+++ screens/Home.js
@@ -1,7 +1,7 @@
 import { View, Text, Image, StyleSheet, TouchableOpacity } from "react-native";
 import React from "react";
 import { SvgUri, SvgXml } from "react-native-svg";
-import {LinearGradient} from 'expo-linear-gradient';
+import { LinearGradient } from "expo-linear-gradient";
 
 export default function Home() {
   const xml = `<?xml version="1.0" encoding="UTF-8"?>
@@ -10,6 +10,11 @@ export default function Home() {
 <path d="M18.7211 26.7178C18.4908 28.5681 16.9126 30 15 30C13.0874 30 11.5092 28.5681 11.2789 26.7178C11.2469 26.4609 11.4599 26.25 11.7188 26.25H18.2812C18.5401 26.25 18.7531 26.4609 18.7211 26.7178Z" fill="#F23535"/>
 </svg>
 `;
+const filterXml=`<?xml version="1.0" encoding="UTF-8"?>
+<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg">
+<path d="M8.89583 17.5V12.8125H10.1458V14.5417H17.5V15.7917H10.1458V17.5H8.89583ZM2.5 15.7917V14.5417H7.64583V15.7917H2.5ZM6.39583 12.3333V10.625H2.5V9.375H6.39583V7.625H7.64583V12.3333H6.39583ZM8.89583 10.625V9.375H17.5V10.625H8.89583ZM12.3542 7.1875V2.5H13.6042V4.20833H17.5V5.45833H13.6042V7.1875H12.3542ZM2.5 5.45833V4.20833H11.1042V5.45833H2.5Z" fill="black"/>
+</svg>
+`
   return (
     <View>
       <View
@@ -33,7 +38,7 @@ export default function Home() {
           </Text>
         </View>
       </View>
-      <View style={{}}>
+      <View style={{ marginVertical: "5%" }}>
         <TouchableOpacity style={styles.button} onPress={() => {}}>
           <View style={{ flexDirection: "row", marginHorizontal: 8 }}>
             <View
@@ -43,11 +48,14 @@ export default function Home() {
                 borderRadius: 20,
               }}
             >
-              <LinearGradient style={{borderRadius:20}} colors={['#30CAC1','#C5FE7E','#F7FB57']}>
-              <Image
-                source={require("../assets/notes.png")}
-                style={{ margin: 6 }}
-              />
+              <LinearGradient
+                style={{ borderRadius: 20 }}
+                colors={["#30CAC1", "#C5FE7E", "#F7FB57"]}
+              >
+                <Image
+                  source={require("../assets/notes.png")}
+                  style={{ margin: 6 }}
+                />
               </LinearGradient>
             </View>
             <Text style={styles.btnText}>Find Labourers</Text>
@@ -57,7 +65,7 @@ export default function Home() {
                 width: 40,
                 height: 40,
                 borderRadius: 20,
-                left:70
+                left: 70,
               }}
             >
               <Image
@@ -67,6 +75,10 @@ export default function Home() {
             </TouchableOpacity>
           </View>
         </TouchableOpacity>
+      </View>
+      <View style={{flexDirection:"row",justifyContent:"space-between",marginHorizontal:"4%",marginTop:"6%"}}>
+        <Text style={{fontSize:15,fontWeight:"bold"}}>News</Text>
+        <SvgXml width="20" height="20" xml={filterXml} />
       </View>
     </View>
   );
